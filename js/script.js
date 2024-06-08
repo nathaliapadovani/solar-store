7e67ee579d041941ca32563f196d363394649472
Nathalia <nathaliapadovani.contato@gmail.com>
2024-06-08 16:07:12 -0300

    desativar hover ao clicar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const botoes = document.querySelectorAll("botao-introducao");
+
+function removerHoverStyle(e) {
+  e.target.style.backgroundColor = '';
+  e.target.style.padding = '';
+  e.target.style.fontSize = ''; 
+}
+
+botoes.forEach(function(botao) {
+  botao.addEventListener("mouseover", removerHoverStyle);
+});
+});
