f0458db3ef8a5fed4428ea71ae64a111dc9b1ef0
Nathalia <nathaliapadovani.contato@gmail.com>
2024-06-08 17:07:35 -0300

    removido estilos de hover dos botoes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,3 @@ if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Seleciona todos os botões com as classes .botao-introducao e .botao-produtos
-  const botoes = document.querySelectorAll(".botao-introducao, .botao-produtos");
-
-function removerHoverStyle(e) {
-  console.log('Hover removido do botão:', e.target);
-  e.target.style.backgroundColor = '';
-  e.target.style.padding = '';
-  e.target.style.fontSize = ''; 
-}
-
-botoes.forEach(function(botao) {
-  botao.addEventListener("mouseover", removerHoverStyle);
-});
-});
